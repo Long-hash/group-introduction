0e78aac8bd787d7968ef279dfcd42e9b4d99e88d
Long <1444715578@qq.com>
2020-03-27 22:57:28 +0800

    add
    add

--- home/贾子安/贾子安.js
+++ home/贾子安/贾子安.js
@@ -1,16 +1,23 @@
 Page({
-    mixins: [require('../../mixin/themeChanged')],
-    data: {
-        gallery: false,
-    },
-    close: function() {
-        this.setData({
-            gallery: false,
-        });
-    },
-    open: function () {
-        this.setData({
-            gallery: true
-        });
-    },
-});
+  mixins: [require('../../mixin/themeChanged')],
+  data: {
+    dialog1: false,
+    dialog2: false
+  },
+  close: function () {
+    this.setData({
+      dialog1: false,
+      dialog2: false
+    });
+  },
+  open1() {
+    this.setData({
+      dialog1: true
+    });
+  },
+  open2() {
+    this.setData({
+      dialog2: true
+    });
+  }
+})
